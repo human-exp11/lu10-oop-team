756e2cc083ec3ecd103bb546a926e3153bd5d39b
Luca Urbanowicz <human.exp11@gmail.com>
2022-03-06 13:41:28 -0700

    Manager testing passed

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,1 +1,20 @@
 const Manager = require("../lib/Manager");
+
+test("To create an office number.", () => {
+    const testOfficeNumber = 2;
+    const employeeTest = new Manager("Luca", 11, "luca@gmail.com", testOfficeNumber);
+    expect(employeeTest.officeNumber).toBe(testOfficeNumber);
+});
+
+test("Testing officeNumber will return office number.", () => {
+    const testOfficeNumber = 2;
+    const employeeTest = new Manager("Luca", 11, "luca@gmail.com", testOfficeNumber);
+    expect(employeeTest.getOfficeNumber()).toBe(testOfficeNumber);
+});
+
+test("Testing role.", () => {
+    const returnValue = "Manager";
+    const employeeTest = new Manager("Luca", 11, "luca@gmail.com", 555-555-5555);
+    expect(employeeTest.getRole()).toBe(returnValue);
+});
+
