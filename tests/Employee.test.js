3272e56826e551430632426aee6bb2ca70e2fb30
Luca Urbanowicz <human.exp11@gmail.com>
2022-03-06 13:33:24 -0700

    Engineer testing passed

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,5 +1,6 @@
 const Employee = require("../lib/Employee");
 
+//Testing the Employee
 test("To create an new employee.", () => {
     const employeeTest = new Employee();
     expect(typeof(employeeTest)).toBe("object");
@@ -23,21 +24,19 @@ test("Testing email.", () => {
     expect(employeeTest.email).toBe(email);
 })
 
-
-
 test("Gets name via getName method.", () => {
     const testName = "Luca";
     const employeeTest = new Employee(testName);
     expect(employeeTest.getName()).toBe(testName);
 })
 
-test("Can test ID via getID method.", () => {
+test("Test ID via getID method.", () => {
     const testID = 11;
     const employeeTest = new Employee("Luca", testID);
     expect(employeeTest.getId()).toBe(testID);
 })
 
-test("Can test email via getEmail method.", () => {
+test("Test email via getEmail method.", () => {
     const testEmail = "luca@gmail.com";
     const employeeTest = new Employee("Luca", 11, testEmail);
     expect(employeeTest.getEmail()).toBe(testEmail);
--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,1 +1,21 @@
 const Engineer = require("../lib/Engineer");
+
+test("To create a github.", () => {
+    const testGithub = "luca_u";
+    const employeeTest = new Engineer("Luca", 11, "luca@gmail.com", testGithub);
+    expect(employeeTest.github).toBe(testGithub);
+});
+
+test("Testing getGithub will return github.", () => {
+    const testGithub = "luca_u";
+    const employeeTest = new Engineer("Luca", 11, "luca@gmail.com", testGithub);
+    expect(employeeTest.getGithub()).toBe(testGithub);
+});
+
+test("Testing role.", () => {
+    const returnValue = "Engineer";
+    const employeeTest = new Engineer("Luca", 11, "luca@gmail.com", "luca_u");
+    expect(employeeTest.getRole()).toBe(returnValue);
+});
+
+
