8aab887b225f3ff125068b0c39d5680d1e23ac54
Luca Urbanowicz <human.exp11@gmail.com>
2022-03-06 13:38:12 -0700

    Intern testing passed

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,1 +1,21 @@
 const Intern = require("../lib/Intern");
+
+test("To create school.", () => {
+    const testSchool = "School Name";
+    const employeeTest = new Intern("Luca", 11, "luca@gmail.com", testSchool);
+    expect(employeeTest.school).toBe(testSchool);
+});
+
+test("Testing getSchool  will return school.", () => {
+    const testSchool = "School Name";
+    const employeeTest = new Intern("Luca",11, "luca@gmail.com", testSchool);
+    expect(employeeTest.getSchool()).toBe(testSchool);
+});
+
+test("Testing role.", () => {
+    const returnValue = "Intern";
+    const employeeTest = new Intern("Luca", 11, "luca@gmail.com", "School Name");
+    expect(employeeTest.getRole()).toBe(returnValue);
+});
+
+
